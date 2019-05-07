239e6d258ca291625d6ee467d0974e174118935f
hana Charaabi <charaabihana90@gmail.com>
2019-05-07 15:08:29 +0100

    Updates

---
+++ precache-manifest.ada9ab473734a48f7a743feca363dd6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98622b4c506490fbe43e565c9e1d4457",
+    "url": "/credit-card/index.html"
+  },
+  {
+    "revision": "5abf2bc5734f102bc0e4",
+    "url": "/credit-card/static/css/main.2c3d6b61.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/credit-card/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "5abf2bc5734f102bc0e4",
+    "url": "/credit-card/static/js/main.67eb079e.chunk.js"
+  },
+  {
+    "revision": "2a4b22a3002466356f44",
+    "url": "/credit-card/static/js/runtime~main.0914b2aa.js"
+  },
+  {
+    "revision": "f69d01d0330b09a4ed11b45e3f42ec29",
+    "url": "/credit-card/static/media/MasterCard.f69d01d0.png"
+  },
+  {
+    "revision": "6ac2533ae68698a1ffa4ae3f8ab17bf3",
+    "url": "/credit-card/static/media/creditCart.6ac2533a.png"
+  },
+  {
+    "revision": "97a3019713284167f8e679c1c53f3c8e",
+    "url": "/credit-card/static/media/puce.97a30197.png"
+  }
+]);
